49b0288a8bf82bcc1a8532f5c2023f86357b597f
Cian Gannon <lego@live.ie>
2017-10-04 17:12:07 +0100

    Question 1

--- Question1.go
+++ Question1.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
+	// Outputs "Hello World!" and hellow world in japanses ("こんにちは世界") to the CMD
 	fmt.Println("Hello World!, \u3053\u3093\u306B\u3061\u306F\u4E16\u754C!")
 }
